02814a52f16bd6a64077482ccf6627644eb1f66e
dmanrica7 <72221055+dmanrica7@users.noreply.github.com>
2021-09-10 14:11:58 +0200

    front listado hoteles

--- Front/frontalapp/src/app/components/filtrado/filtrado.component.ts
+++ Front/frontalapp/src/app/components/filtrado/filtrado.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { MapInfoWindow, MapMarker } from '@angular/google-maps';
+import { Component, OnInit } from '@angular/core';
+import { Hotel } from 'src/app/models/hotel.model';
 
 @Component({
   selector: 'app-filtrado',
@@ -8,51 +8,70 @@ import { MapInfoWindow, MapMarker } from '@angular/google-maps';
 })
 export class FiltradoComponent implements OnInit {
 
-  @ViewChild(MapInfoWindow, {static: false}) infoWindow: any;
-
   currentCity = "Tarragona";
   currentCenter = {
     lat: 41.11667,
     lng: 1.25
   }
 
-  markers = [{
-      position: {
-        lat: 41.115248,
-        lng: 1.256901
-      },
-      label: {
-        color: 'yellow',
-        text: 'H10'
-      },
-      clickeable: true,
-      options: {
-        animation: google.maps.Animation.DROP
-      }
+  listaHotels: Array<any> = [
+    {
+      id_hotel: 2,
+      nombre: 'H10',
+      categoria: '4',
+      ubi_lat: 41.115248,
+      ubi_long: 1.256901,
+      precio_noche: 80,
+      valoracion: 'excelente',
+      imagen: 'url',
+      telefono: '900400466',
+      id_poblacion: 1,
     },
     {
-      position: {
-        lat: 41.117412,
-        lng: 1.265011
-      },
-      label: {
-        color: 'yellow',
-        text: 'Astari'
-      },
-      clickeable: true,
-      options: {
-        animation: google.maps.Animation.DROP
-      }
+      id_hotel: 4,
+      nombre: 'Astari',
+      categoria: '3',
+      ubi_lat: 41.117412,
+      ubi_long: 1.265011,
+      precio_noche: 80,
+      valoracion: 'muybien',
+      imagen: 'url',
+      telefono: '977236900',
+      id_poblacion: 1
     }
-  ]
+  ];
+
+  listaDistancias: Array<any> = []
+
+  markers: Array<any> = []
 
   constructor() { }
 
   ngOnInit(): void {
+    let i = 0;
+    while(i<this.listaHotels.length){
+      this.listaDistancias.push(Math.round(this.getDistancia(this.listaHotels[i].ubi_lat, this.listaHotels[i].ubi_long)*100)/100);
+      this.markers.push({
+        position: {
+          lat: this.listaHotels[i].ubi_lat,
+          lng: this.listaHotels[i].ubi_long
+        },
+        label: {
+          color: 'yellow',
+          text: this.listaHotels[i].nombre
+        },
+        clickeable: true,
+        options: {
+          animation: google.maps.Animation.DROP
+        }
+      });
+      i++;
+    }
   }
 
-  openInfo() {
-    window.open("/busqueda");
+  getDistancia(lat: number, long: number){
+    console.log(this.listaHotels[0].nombre);
+    return Math.sqrt(Math.pow(this.currentCenter.lat-lat, 2)+Math.pow(this.currentCenter.lng-long, 2));
   }
 
 }
--- Front/frontalapp/src/app/models/hotel.model.ts
+++ Front/frontalapp/src/app/models/hotel.model.ts
@@ -1,2 +1,14 @@
+import { Poblacion } from "./poblacion.model";
+
 export class Hotel {
+  id_hotel?: number;
+  nombre?: string;
+  categoria?: string;
+  ubi_lat?: number;
+  ubi_long?: number;
+  precio_noche?: number;
+  valoracion?: string;
+  imagen?: string;
+  telefono?: string;
+  id_poblacion?: number;
 }
